808c18a4e30455fe5adb40f8da5515c1f9d6726f
Michael <Michael@MichaelHumiston.com>
2015-06-13 12:57:39 +1000

    Save Title
    Created date format
    Set session title on creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,10 @@ class SessionsController < ApplicationController
 	def index
 		@sessions = Session.all
 	end
+	
 	def new
 		@session = Session.new
+		@session.title = Date.current.to_s(:month_day_and_year)
 		@session.save
 		
 		redirect_to @session
