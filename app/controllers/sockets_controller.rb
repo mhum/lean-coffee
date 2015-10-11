8775049d6f11352c90bcdd566fb1549435385662
Michael <Michael@MichaelHumiston.com>
2015-10-11 17:41:21 +1100

    Prevent multiple users from dragging topic
    - Prevent multiple users from dragging same topic at same time
    - Add visual style to a topic when it is being dragged

--- app/controllers/sockets_controller.rb
+++ app/controllers/sockets_controller.rb
@@ -1,7 +1,16 @@
 class SocketsController < WebsocketRails::BaseController
 
+	# Start move topic around screen
+	def move_topic_start
+		WebsocketRails[(message[:session]).to_s.to_sym].trigger(:move_topic_start, message)
+	end
+
 	# Move topic around screen
 	def move_topic
 		WebsocketRails[(message[:session]).to_s.to_sym].trigger(:move_topic, message)
+	end
+	# Stop move topic around screen
+	def move_topic_stop
+		WebsocketRails[(message[:session]).to_s.to_sym].trigger(:move_topic_stop, message)
 	end
 end
